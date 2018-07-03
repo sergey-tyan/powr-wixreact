d6177e86dfc9cabb601dde94abc314949e78068c
sergey <sergey@powr.io>
2018-07-03 11:28:44 -0700

    export wix.min

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-// window.$ = require('./jquery-3.1.1');
-// window.jQuery = window.$;
-require('./wix.min');
-// require('./editor-ui-lib-jquery');
-// require('./wix.min');
-// window.WixReact = require('./editor-ui-lib');
-
 module.exports = {
-  WixMin: require('./wix.min'),
   WixReact: require('react'),
   WixReactLib: require('./editor-ui-lib'),
   WixReactDOM: require('react-dom')
